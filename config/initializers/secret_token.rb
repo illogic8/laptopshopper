a872b53b0750dc2798e54f3da5b32b6b9000a908
Eric <eswim007@yahoo.com>
2013-05-13 13:42:34 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Laptopshopper::Application.config.secret_token = '3d58393db304af351ace7411cba6131ff1e890e246cd6918cbbeadc622a66f3450c5c50cf51cc5e94cb46dede6082e16b8530c537d0f6d7633eca075f7442a34'
